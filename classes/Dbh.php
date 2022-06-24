20d4683727644686e255d55f3626193f4617fe23
oyinkansola <yahayaoyinkansola@gmail.com>
2022-06-24 13:13:21 +0100

    Added class for database connection

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -1,3 +1,20 @@
 <?php
 
+class Dbh
+{
+    private $hostname = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "zuriphp";
 
+    protected function connect()
+    {
+        $this->connection = new mysqli($this->hostname, $this->username, $this->password, $this->dbname);
+
+        if (is_null($this->connection->connect_error)) {
+            return $this->connection;
+        } else {
+            echo "Connection failed -> " . $this->connection->connect_error;
+        }
+    }
+}
